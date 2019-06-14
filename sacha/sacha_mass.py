a7e0cf1edf7d7719561d631404afd83b02b24e75
Georg Petz <gpetz@users.noreply.github.com>
2019-06-14 14:09:41 +0200

    download txt and hocr in seperate files

--- sacha/sacha_mass.py
+++ sacha/sacha_mass.py
@@ -7,6 +7,7 @@ import json
 import logging
 import requests
 import time
+import pathlib
 from multiprocessing import Pool, cpu_count
 
 logging.basicConfig(handlers=[logging.FileHandler('sacha_multi.log'),
@@ -37,18 +38,32 @@ def download(barcode):
             pages = len(manifest['sequences'][0]['canvases'])
             # for canvas in tqdm(manifest['sequences'][0]['canvases']):
             for canvas in manifest['sequences'][0]['canvases']:
-                content = canvas['otherContent'][0]['resources'][0]['resource']['@id']
-                if content[-3:] == 'txt':
+                content_txt = canvas['otherContent'][0]['resources'][0]['resource']['@id']
+                if content_txt[-3:] == 'txt':
                     # canvas_text = requests.get(content)
                     # quick fix for ONB switch to https, as this is not changed in the manifests yet
-                    canvas_text = requests.get('https' + content[4:])
+                    canvas_text = requests.get('https' + content_txt[4:])
                     status = canvas_text.status_code
                     if status == 200:
+                        pathlib.Path('%s/books/%s' % (target_dir, barcode[1:])).mkdir(exist_ok=True)
+                        with open('%s/books/%s/%s' % (target_dir, barcode[1:], content_txt.split("/")[-1]), 'wb') as f:
+                              f.write(canvas_text.content)
                         pass
                     else:
-                        logging.critical('%s - requesting produced a HTTP %s' % (content, status))
+                        logging.critical('%s - requesting produced a HTTP %s' % (content_txt, status))
                     fulltext.write(canvas_text.text)
                     fulltext.write('\n')
+                content2 = canvas['otherContent'][0]['resources'][1]['resource']['@id']
+                if content2[-4:] == 'html':
+                    canvas_text = requests.get('https' + content2[4:])
+                    status = canvas_text.status_code
+                    if status == 200:
+                        pathlib.Path('%s/books/%s' % (target_dir, barcode[1:])).mkdir(exist_ok=True)
+                        with open('%s/books/%s/%s' % (target_dir, barcode[1:], content2.split("/")[-1]), 'wb') as f:
+                            f.write(canvas_text.content)
+                        pass
+                    else:
+                        logging.critical('%s - requesting produced a HTTP %s' % (content2, status))
                 # TODO: change according to the current system!
                 # Aim for a maximum of 400 requests per second, depending on the number of cores available.
                 time.sleep(0.01)
@@ -62,27 +77,38 @@ def download(barcode):
 
 if __name__ == '__main__':
 
+    def callback_error(result):
+        logging.error(result)
+
     # target_dir = '../../tl-classification/data/travelogues-groundtruth/sacha_full/16th_full'
     # csv_file = '../../tl-classification/data/travelogues-groundtruth/sacha_full/15-16-SACHA.csv'
 
     # target_dir = '../../tl-classification/data/travelogues-groundtruth/sacha_full/17th_full'
     # csv_file = '../../tl-classification/data/travelogues-groundtruth/sacha_full/16-17-SACHA.csv'
 
-    target_dir = '../../tl-classification/data/travelogues-groundtruth/sacha_full/18th_full'
-    csv_file = '../../tl-classification/data/travelogues-groundtruth/sacha_full/17-18-SACHA.csv'
+    # target_dir = '../../tl-classification/data/travelogues-groundtruth/sacha_full/18th_full'
+    # csv_file = '../../tl-classification/data/travelogues-groundtruth/sacha_full/17-18-SACHA.csv'
 
     # target_dir = '../../tl-classification/data/travelogues-groundtruth/sacha_full/19th_full'
     # csv_file = '../../tl-classification/data/travelogues-groundtruth/sacha_full/18-1877-SACHA.csv'
 
+    target_dir = '.'
+
     pool = Pool(processes=cpu_count()-1)  # uses all available cores minus 1 (to prevent locking the system)
 
-    with open(csv_file) as csvfile:
-        # read .csv content
-        csv_reader = csv.DictReader(csvfile, delimiter=',')
+    # with open(csv_file) as csvfile:
+    #     # read .csv content
+    #     csv_reader = csv.DictReader(csvfile, delimiter=',')
+    #
+    #     for row in csv_reader:
+    #         barcode = str(row['Strichcode'])
+    #         pool.apply_async(download, args=(barcode,))
 
-        for row in csv_reader:
-            barcode = str(row['Strichcode'])
-            pool.apply_async(download, args=(barcode,))
+    with open('analyzed_min.csv') as f:
+        barcode_list = f.read().splitlines()
+
+    for barcode in barcode_list:
+        pool.apply_async(download, args=(barcode,), error_callback=callback_error)
 
     pool.close()
     pool.join()
